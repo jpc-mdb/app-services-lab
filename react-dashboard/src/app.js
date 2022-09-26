d47f328c16c701d1efa41f7712a9da0ba92496d9
Jean-Philippe Chenot <jean-philippe.chenot@mongodb.com>
2022-09-26 18:25:35 +0100

    Added the bootstrap template and started adding Routing

--- react-dashboard/src/app.js
+++ react-dashboard/src/app.js
@@ -1,7 +1,15 @@
 import React, { Component }  from 'react';
+import './app.css';
+import { Home } from './pages/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function app() {
     return (
-        <h1>Atlas Management Dashboard</h1>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/index.html" element={<Home />} />
+            </Routes>
+        </Router>
     );
 }
---
+++ react-dashboard/src/components/Dashboard/Report.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const GenerateReport = () => {
+    return (
+        <div className="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                    className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+        </div>
+    );
+}
